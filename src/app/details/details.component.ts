b11aeafd44a84d0f6a60abbd24e83ef7a00bee8e
Arjan <arjan@koens.biz>
2020-05-17 17:11:10 +0200

    chart stuff

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  public chartTitle: any = { text: 'overview' };
+  public chartData: number[][] = [[123, 276, 310, 212, 240, 156, 98], [165, 210, 287, 144, 190, 167, 212], [56, 140, 195, 46, 123, 78, 95]];
+
   constructor() { }
 
   ngOnInit(): void {
