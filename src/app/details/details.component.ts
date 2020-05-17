5bbec7e183d2d2616c60aa4dea7b4f7fe9738a71
Arjan <arjan@koens.biz>
2020-05-17 17:26:45 +0200

    power
    money, power and women

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Line } from '../models/line'
 
 @Component({
   selector: 'app-details',
@@ -8,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class DetailsComponent implements OnInit {
 
   public chartTitle: any = { text: 'overview' };
-  public chartData: number[][] = [[123, 276, 310, 212, 240, 156, 98], [165, 210, 287, 144, 190, 167, 212], [56, 140, 195, 46, 123, 78, 95]];
-
-  constructor() { }
+  public lines: Line[];
+  
+  constructor() {  }
 
   ngOnInit(): void {
+    this.lines = [
+      {id:0, name: "money", data: [123, 276, 310, 212, 240, 156, 98]},
+      {id:1, name: "power", data: [165, 210, 287, 144, 190, 167, 212]},
+      {id:2, name: "women", data: [56, 140, 195, 46, 123, 78, 95]}
+    ]
   }
 
 }
